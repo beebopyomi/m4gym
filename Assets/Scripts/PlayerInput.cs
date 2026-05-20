76e6abafc4ac1320370cfb23374937fcd16df156
shikanokonokonokokoshitantan <jakubszymanzki@gmail.com>
2026-05-20 11:24:53 +0200

    hi
    hi

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    [SerializeField] private InputActionAsset input;
+    [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float turnSpeed = 150f;
+    [SerializeField] private float jumpForce = 5f;
+    [SerializeField] private string mapName = "Player";
+
+    private InputAction moveAction;
+    private InputAction jumpAction;
+    private InputAction sprintAction;
+
+    private Rigidbody rb;
+    private bool isGrounded = false;
+
+    void Awake()
+    {
+        InputActionMap map = input.FindActionMap(mapName);
+        moveAction  = map.FindAction("walk");
+        jumpAction  = map.FindAction("Jump");
+        sprintAction = map.FindAction("Sprint");
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnEnable()  { input.FindActionMap(mapName).Enable(); }
+    void OnDisable() { input.FindActionMap(mapName).Disable(); }
+
+    void Update()
+    {
+        Vector2 moveInput = moveAction.ReadValue<Vector2>();
+
+        float speed = walkSpeed * moveInput.y;
+
+        if (sprintAction.IsPressed())
+            speed *= 2f;
+
+        Vector3 movement = transform.forward * speed * Time.deltaTime;
+        transform.Translate(movement, Space.World);
+
+        float angle = moveInput.x * turnSpeed * Time.deltaTime;
+        transform.Rotate(0f, angle, 0f, Space.World);
+
+
+        if (jumpAction.WasPressedThisFrame() && isGrounded)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = true;
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+            isGrounded = false;
+    }
+}
